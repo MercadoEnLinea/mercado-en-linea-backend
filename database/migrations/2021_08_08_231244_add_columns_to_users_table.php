2ed4493908b3ac50098c2defaca0cd8f109bd723
Carlos Gomez <contacto@carlosgomez.mx>
2021-08-09 16:05:24 -0500

    fix: fix migration when running on sqlite

--- database/migrations/2021_08_08_231244_add_columns_to_users_table.php
+++ database/migrations/2021_08_08_231244_add_columns_to_users_table.php
@@ -14,7 +14,7 @@ class AddColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->unique();
+            $table->string('phone')->nullable()->unique();
             $table->timestamp('phone_verified_at')->nullable();
             $table->boolean('account_verified')->default(false);
         });
