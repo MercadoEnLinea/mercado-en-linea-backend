9a8c8b7d7beb9db20d8a0dd41a9221dc122b2735
Erick Perez <erickiv1995@gmail.com>
2021-08-09 23:52:52 -0500

    test: added transaction unit test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -22,17 +23,41 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
 
-        $user = User::updateOrCreate([
-            'name' => 'Demo User',
+        $buyer = User::updateOrCreate([
+            'name' => 'Demo Buyer User',
             'email'=>'demo@demo.com',
             'password' => Hash::make('password'),
             'phone' => '0000000000'
+        ]);
+
+
+        $seller = User::updateOrCreate([
+            'name' => 'Demo Seller User',
+            'email'=>'demoSeller@demo.com',
+            'password' => Hash::make('password'),
+            'phone' => '0000000001'
         ]);
 
         $data = [
             'name' => 'Base Test Category',
             'description' => 'Unit Testing Categories'];
         $category = Category::create($data);
+
+
+        $data = [
+            'seller_id' => 1,
+            'name' => 'Unit Base Testing Product',
+            'description' => 'Unit Testing Products',
+            'category_id' => 1,
+            'delivery_options' => ['PERSONAL', 'MAIL'],
+            'payment_options' => ['CASH'],
+            'images' => ['https://carlosgomez.mx/assets/front/img/avatar_1624175972505033680.png']
+
+        ];
+        $product = Product::create($data);
+        $product->delivery_options = ['PERSONAL', 'MAIL'];
+        $product->payment_options = ['CASH'];
+        $product->save();
 
     }
 }
