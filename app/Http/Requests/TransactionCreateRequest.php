4815da830ee1f817d9f9d9c1c8872ff223f4fa8e
Carlos Gomez <contacto@carlosgomez.mx>
2021-08-10 00:27:52 -0500

    feat: improve validations on requests

---
+++ app/Http/Requests/TransactionCreateRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TransactionCreateRequest extends BaseRequest
+{
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'product_id' => 'required|integer|exists:products,id',
+            'quantity' => 'sometimes|integer|min:1'
+        ];
+    }
+}
---
+++ app/Http/Requests/TransactionReviewRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransactionReviewRequest extends BaseRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'score' => 'required|integer|min:1|max:5',
+            'body' => 'required|string',
+
+        ];
+    }
+}
